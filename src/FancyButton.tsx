b216fa97dfba9705f61b87e71cc85536c6118a39
jq.run <dev@jq.run>
2021-04-06 17:17:44 -0400

    try the new stuff

--- src/FancyButton.tsx
+++ src/FancyButton.tsx
@@ -2,6 +2,5 @@ import Button from '@material-ui/core/Button';
 import { FunctionComponent } from 'react';
 
 export const FancyButton: FunctionComponent = ({ children }) => {
-  // return <Button sx={{ background: 'red' }}>{children}</Button>;
-  return <Button>{children}</Button>;
+  return <Button sx={{ background: 'red' }}>{children}</Button>;
 };
--- src/FancyProvider.tsx
+++ src/FancyProvider.tsx
@@ -1,15 +1,16 @@
-import { Box, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { FunctionComponent } from 'react';
+import Box from '@material-ui/core/Box';
 
 export const FancyProvider: FunctionComponent = ({ children }) => {
   return (
     <ThemeProvider theme={createMuiTheme({})}>
       <Box
-      // sx={{
-      //   width: '100%',
-      //   height: '100%',
-      //   background: 'gray',
-      // }}
+        sx={{
+          width: '100%',
+          height: '100%',
+          background: 'gray',
+        }}
       >
         {children}
       </Box>
